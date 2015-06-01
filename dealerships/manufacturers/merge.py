677c8faa9300f9fde0a770f0416f2f4fbf534665
Simon Wenmouth <simon.wenmouth@gmail.com>
2015-05-31 21:57:03 -0500

    Initial version of merged normalized data (and its script)

---
+++ dealerships/manufacturers/merge.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import os
+import glob
+import json
+
+# declare input and output file names
+
+directory_name=os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'data'))
+
+input_names=glob.glob(os.path.join(directory_name, 'normalized-*.json'))
+
+output_name=os.path.join(directory_name, 'normalized.json')
+
+dealers = {}
+
+for input_name in input_names:
+    with open(input_name, 'r') as fd:
+        items = json.load(fd)
+        for item in items:
+            if 'url' in item:
+                url = item['url'].replace('http://', '').lower()
+                if not url in dealers:
+                    dealers[url] = item
+            else:
+                name = item['name'].lower()
+                if not name in dealers:
+                    dealers[name] = item
+
+with open(output_name, 'w') as fd:
+    json.dump(list(dealers.values()), fd)
+
