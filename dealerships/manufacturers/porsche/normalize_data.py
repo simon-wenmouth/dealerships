a4560a2a34395e1bafb45cac6e3cfe80158779f5
Simon Wenmouth <simon.wenmouth@gmail.com>
2015-06-01 20:00:24 -0500

    normalize address region (i.e. us state name to code)

--- dealerships/manufacturers/porsche/normalize_data.py
+++ dealerships/manufacturers/porsche/normalize_data.py
@@ -87,14 +87,70 @@ def ToOpeningHoursSpecification(text):
     except:
         return None
 
+us_state_abbrev = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY',
+}
+
 businesses = []
 
 with open(input_name, 'r') as fd:
     dealers = json.load(fd)
     for dealer in dealers:
+        region = dealer['address']['region']
+        if region in us_state_abbrev:
+            region = us_state_abbrev[region]
         address = Address(
             streetAddress   = dealer['address']['street'],
-            addressRegion   = dealer['address']['region'],
+            addressRegion   = region,
             postalCode      = dealer['address']['postalcode'],
             addressLocality = dealer['address']['city'],
             addressCountry  = 'US'
